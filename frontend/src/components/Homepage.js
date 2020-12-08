0abe496554e20355e15082227cac23982e7d6a0e
msaaddev <mrsaadirfan@gmail.com>
2020-12-08 19:33:42 +0500

    📦 NEW: item cards

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,11 +1,13 @@
 import '../styles/homepage.css';
+import shoe from '../images/shoe.png';
+import ItemCard from './common/ItemCard';
 
 const Homepage = () => {
 	return (
 		<div className="hmpg-container">
 			<div className="hmpg-header">
 				<div className="hmpg-title">
-					<h2>shophub</h2>
+					<h2>donatehub</h2>
 				</div>
 				<div className="hmpg-nav-link">
 					<a href="">Home</a>
@@ -17,7 +19,31 @@ const Homepage = () => {
 					<button>donate</button>
 				</div>
 			</div>
-			<div className="hmpg-body"></div>
+			<div className="hmpg-body">
+				<div className="hmpg-sub-container">
+					<div className="hmpg-sub-containers-items">
+						<p>
+							<span>Home</span>/Items
+						</p>
+						<h2>Items</h2>
+						<div className="hmpg-sorting-options">
+							<select>
+								<option value="date">Newest</option>
+								<option value="clothes">Clothes</option>
+								<option value="electronics">Electronic</option>
+							</select>
+						</div>
+					</div>
+					<div className="hmpg-sub-containers-cards">
+						<ItemCard img={shoe} itemName="Shoe" />
+						<ItemCard img={shoe} itemName="Shoe" />
+						<ItemCard img={shoe} itemName="Shoe" />
+						<ItemCard img={shoe} itemName="Shoe" />
+						<ItemCard img={shoe} itemName="Shoe" />
+						<ItemCard img={shoe} itemName="Shoe" />
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
---
+++ frontend/src/components/common/ItemCard.js
@@ -0,0 +1,21 @@
+import '../../styles/itemcard.css';
+
+const ItemCard = ({ itemName, img }) => {
+	return (
+		<div className="itmcard-item-cards">
+			<div className="itmcard-item-bgcolors">
+				<div className="itmcard-item-circle"></div>
+			</div>
+			<div className="itmcard-item-image-container">
+				<div className="itmcard-item-img">
+					<img src={img} alt={img} />
+				</div>
+			</div>
+			<div className="itmcard-item-name">
+				<h2>{itemName}</h2>
+			</div>
+		</div>
+	);
+};
+
+export default ItemCard;
